faef35398a0db405fb1cd66212aeabe5a55aa042
George Hieron <george.hieron@gmail.com>
2021-01-17 00:13:47 +0200

    Horizontal scroll from tablet only and resize checking

--- src/js/horizontal-scroll.js
+++ src/js/horizontal-scroll.js
@@ -1,13 +1,28 @@
 document.addEventListener(
     "DOMContentLoaded",
     function () {
-        document.getElementById("site-wrap").addEventListener("wheel", function (event) {
-            if (event.deltaY > 0) {
-                this.scrollLeft += event.deltaY;
-            } else {
-                this.scrollLeft += event.deltaY;
+        var resizeTimer; // Set resizeTimer to empty so it resets on page load
+
+        function resizeFunction() {
+            if (window.innerWidth > 768) {
+                document.getElementById("site-wrap").addEventListener("wheel", function (event) {
+                    if (event.deltaY > 0) {
+                        this.scrollLeft += event.deltaY;
+                    } else {
+                        this.scrollLeft += event.deltaY;
+                    }
+                });
             }
+        };
+    
+        // On resize, run the function and reset the timeout
+        // 250 is the delay in milliseconds.
+        window.addEventListener("resize", function() {
+            clearTimeout(resizeTimer);
+            resizeTimer = setTimeout(resizeFunction, 250);
         });
+
+        resizeFunction();
     },
     false
 );
