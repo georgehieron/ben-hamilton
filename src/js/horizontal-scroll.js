5459a0c14e267d10e9d490f251b940b5b359522c
George Hieron <george.hieron@gmail.com>
2021-01-23 20:40:57 +0200

    Tidy locomotive implementation

--- src/js/horizontal-scroll.js
+++ src/js/horizontal-scroll.js
@@ -1,49 +1,24 @@
 document.addEventListener(
     "DOMContentLoaded",
     function () {
-        // var resizeTimer; // Set resizeTimer to empty so it resets on page load
-
-        // function resizeFunction() {
-        //     if (window.innerWidth > 768) {
-        //         const target = document.getElementById("site-wrap");
-
-        //         target.addEventListener('wheel', e => {
-        //             const toLeft  = e.deltaY < 0 && target.scrollLeft > 0
-        //             const toRight = e.deltaY > 0 && target.scrollLeft < target.scrollWidth - target.clientWidth
-        //             const toTop  = e.deltaX < 0 && target.scrollLeft > 0
-        //             const toBottom = e.deltaX > 0 && target.scrollLeft < target.scrollWidth - target.clientWidth
-
-        //             // Scroll horizontall on vertical scroll
-        //             if (toLeft || toRight) {
-        //                 e.preventDefault()
-        //                 e.stopPropagation()
-        //                 target.scrollLeft += e.deltaY / 4;
-        //             }
-
-        //             // Prevent actual horizontal scrolling with a trackpad
-        //             if (toTop || toBottom) {
-        //                 e.preventDefault()
-        //                 e.stopPropagation()
-        //                 target.scrollTop += e.deltaX;
-        //             }
-        //         })
-        //     }
-        // }
-
-        // // On resize, run the function and reset the timeout
-        // // 250 is the delay in milliseconds.
-        // window.addEventListener("resize", function () {
-        //     clearTimeout(resizeTimer);
-        //     resizeTimer = setTimeout(resizeFunction, 250);
-        // });
-
-        // resizeFunction();
-
         const scroller = new LocomotiveScroll({
             el: document.querySelector('[data-scroll-container]'),
             smooth: true,
-            direction: "horizontal"
-          });
+            direction: 'horizontal',
+            tablet: {
+                smooth: false,
+                direction: 'vertical',
+                gestureDirection:"vertical",
+                breakpoint: 992
+            },
+            smartphone: {
+                smooth: false,
+                direction: 'vertical',
+                gestureDirection:"vertical"
+            },
+            reloadOnContextChange: true,
+            resetNativeScroll: false
+        });
     },
     false
 );
