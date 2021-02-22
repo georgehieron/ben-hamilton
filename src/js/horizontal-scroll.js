9cba825f7f3f885c25b8ec5f312d7d28067ae6e9
George Hieron <george.hieron@gmail.com>
2021-02-22 20:39:49 +0200

    Tidy JS

--- src/js/horizontal-scroll.js
+++ src/js/horizontal-scroll.js
@@ -1,35 +1,34 @@
 document.addEventListener(
     "DOMContentLoaded",
     function () {
-
-        gsap.registerPlugin(ScrollTrigger,Draggable);
+        gsap.registerPlugin(ScrollTrigger, Draggable);
 
         var scroller = document.getElementById("main-content");
 
         var scrollWidth;
-        
+
         var proxy = document.createElement("div");
         var draggable = Draggable.create(proxy, {
-        trigger: scroller,
-        type: "x",
-        dragResistance: -1,
-        onDrag: function() {
-            trigger.scroll(-this.x);
-        }
+            trigger: scroller,
+            type: "x",
+            dragResistance: -1,
+            onDrag: function () {
+                trigger.scroll(-this.x);
+            },
         })[0];
 
         function createScroller() {
-        scrollWidth = scroller.offsetWidth - innerWidth;
-        draggable.applyBounds({minX: -scrollWidth, maxX: 0});
-        ScrollTrigger.addEventListener("refreshInit", () => {
             scrollWidth = scroller.offsetWidth - innerWidth;
-            gsap.set(document.body, {height: scrollWidth + innerHeight});
-        });
+            draggable.applyBounds({ minX: -scrollWidth, maxX: 0 });
+            ScrollTrigger.addEventListener("refreshInit", () => {
+                scrollWidth = scroller.offsetWidth - innerWidth;
+                gsap.set(document.body, { height: scrollWidth + innerHeight });
+            });
         }
-        
+
         function updateProxy() {
             // move the handler to the corresponding ratio according to the page's scroll position.
-            gsap.set(proxy, {x: -trigger.scroll()});
+            gsap.set(proxy, { x: -trigger.scroll() });
         }
 
         createScroller();
@@ -37,44 +36,38 @@ document.addEventListener(
         var trigger;
 
         ScrollTrigger.matchMedia({
-            
             // desktop
-            "(min-width: 64em)": function() {
+            "(min-width: 64em)": function () {
+                // Add keyboard navigation
+                document.addEventListener("keyup", function (event) {
+                    if (event.code == "Tab") {
+                        let activeRect = document.activeElement.offsetParent;
+                        trigger.scroll(activeRect.offsetLeft - 150);
+                    }
+                    if (event.shiftKey && event.code == "Tab") {
+                        let activeRect = document.activeElement.offsetParent;
+                        trigger.scroll(activeRect.offsetLeft - 150);
+                    }
+                });
 
-            // Add keyboard navigation
-            document.addEventListener('keyup', function(event) {
-                if (event.code == 'Tab') {
-                    let activeRect = document.activeElement.offsetParent;
-                    trigger.scroll((activeRect.offsetLeft - 150));
-                }
-                if(event.shiftKey && event.code == 'Tab') {
-                    let activeRect = document.activeElement.offsetParent;
-                    trigger.scroll((activeRect.offsetLeft - 150));
-                }
-            });
-            
-            // gsap.set(document.body, {height: scrollWidth + innerHeight});
-            var anim = gsap.to(scroller, {
-                x: () => -((scroller.scrollWidth - (innerWidth)) / 16) + "rem",
-                ease: "none", 
-            });
-            
-            trigger = ScrollTrigger.create({
-                animation: anim,
-                id: 'tlScroller',
-                scrub: 2,
-                start: "left left",
-                end: "bottom bottom",
-                // start: 0,
-                // end: scrollWidth,
-                // horizontal: true,
-                // markers: true,
-                invalidateOnRefresh: true,
-                onUpdate: updateProxy
-            });
+                // gsap.set(document.body, {height: scrollWidth + innerHeight});
+                var anim = gsap.to(scroller, {
+                    x: () =>
+                        -((scroller.scrollWidth - innerWidth) / 16) + "rem",
+                    ease: "none",
+                });
 
-            }
-        })
+                trigger = ScrollTrigger.create({
+                    animation: anim,
+                    id: "tlScroller",
+                    scrub: 2,
+                    start: 0,
+                    end: scrollWidth,
+                    invalidateOnRefresh: true,
+                    onUpdate: updateProxy,
+                });
+            },
+        });
     },
     false
 );
