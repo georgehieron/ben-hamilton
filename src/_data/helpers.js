cad66eb6c4e72647be80201412c18947f155db9a
George Hieron <george.hieron@gmail.com>
2021-03-05 23:31:22 +0200

    Basic next work item markup & styles

---
+++ src/_data/helpers.js
@@ -0,0 +1,24 @@
+module.exports = {
+    /**
+     * Filters out the passed item from the passed collection
+     * and randomises and limits them based on flags
+     *
+     * @param {Array} collection The 11ty collection we want to take from
+     * @param {Object} item The item we want to exclude (often current page)
+     * @returns {Array} The resulting collection
+     */
+    getNextItem(collection, item) {
+        let nextItem = [];
+        for (let i = 0; i < collection.length; i++) {
+            if (collection[i].url === item.url) {
+                if (i === collection.length - 1) {
+                    nextItem.push(collection[0]);
+                } else {
+                    nextItem.push(collection[i + 1]);
+                }
+            }
+        }
+
+        return nextItem;
+    },
+};
